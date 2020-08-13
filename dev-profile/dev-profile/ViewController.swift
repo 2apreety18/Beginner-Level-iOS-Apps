0d3f191461f01f9a5129a3ec1e3e773039f0134e
Afia Anjum Preety <2apreety@gmail.com>
2020-08-13 19:33:44 +0600

    ipad support

---
+++ dev-profile/dev-profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by preety on 12/8/20.
+//  Copyright © 2020 Preety. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
